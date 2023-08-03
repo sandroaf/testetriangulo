81bc93c7ef300a641fe64f247650c3a06681ba7c
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-08-03 16:56:25 -0300

    Desenvolvida primeira versão funcional do exercicio

---
+++ triangulo.js
@@ -0,0 +1,15 @@
+function fTestaTriangulo() {
+    const l1 = parseFloat(document.querySelector("#iLado1").value);
+    const l2 = parseFloat(document.querySelector("#iLado2").value);
+    const l3 = parseFloat(document.querySelector("#iLado3").value);
+    if ((l1 + l2 > l3) && 
+        (l1 + l3 > l2) &&
+        (l2 + l3 > l1)) {
+            document.querySelector("#pResultado").innerHTML = "Esses lados representam um Triangulo";
+            document.querySelector("#pResultado").style.color = "green";
+        } else {
+            document.querySelector("#pResultado").innerHTML = "Essas lados não formam um Triangulo;"
+            document.querySelector("#pResultado").style.color = "red";
+        }
+    return false; //Não enviar para a Action do Form
+}
